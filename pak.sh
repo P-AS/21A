a5c8a105eaac9841333b7bcbbf70ea2069342b04
PAS <pasrc@proton.me>
2025-10-13 00:47:16 -0400

    lets get this shit started

---
+++ pak.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Packs (compresses) or unpacks (extracts) a ltm movie.
+# Usage: ./pak.sh u/p archive.ltm
+
+archive="$2"
+
+if [ $1 = "p" ]; then
+	echo Packing LTM "$archive"
+	tar -cvzf "$archive" config.ini inputs #annotations.txt editor.ini
+elif [ $1 = "u" ]; then
+	echo Unpaking LTM "$archive"
+	tar -xvzf "$archive"
+fi
